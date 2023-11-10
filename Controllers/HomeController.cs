af885e718b10416288d18ce276b3c7907233cdf3
yanne23 <yannella_lopez@usmp.pe>
2023-11-09 21:55:09 -1200

    myMLApp - funcional

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,31 +1,57 @@
-﻿using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.ML;
+using System.Diagnostics;
+
 using practica_n4.Models;
 
-namespace practica_n4.Controllers;
-
-public class HomeController : Controller
+namespace practica_n4.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-        _logger = logger;
-    }
+        private readonly ILogger<HomeController> _logger;
+        private readonly PredictionEngine<SentimentModel.ModelInput, SentimentModel.ModelOutput> _predictionEngine;
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+            // Configurar el motor de predicción con el modelo entrenado
+            var mlContext = new MLContext();
+            var mlModel = mlContext.Model.Load(SentimentModel.MLNetModelPath, out var _);
+            _predictionEngine = mlContext.Model.CreatePredictionEngine<SentimentModel.ModelInput, SentimentModel.ModelOutput>(mlModel);
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+public IActionResult AnalyzeSentiment(string text)
+{
+    // Realizar la predicción de sentimiento
+    var input = new SentimentModel.ModelInput { Col0 = text };
+    var prediction = SentimentModel.Predict(input);
+
+    // Almacenar el resultado en ViewBag para mostrar en la vista
+    ViewBag.Sentiment = prediction.PredictedLabel;
+
+    // Volver a la vista Index para mostrar el resultado
+    return View("Index");
+}
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
+
+
